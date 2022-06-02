a155c45244de028b1c2daa036bf181261e04c7b3
DhurimZz <93375353+DhurimZz@users.noreply.github.com>
2022-06-02 17:00:08 +0200

    CRUD DOCTORS
    Update Doctor(Perditesimi i doktorave)
    
    Co-Authored-By: ArbnorLA <94640226+ArbnorLA@users.noreply.github.com>

--- SistemPerMenaxhiminESpitalit/SistemPerMenaxhiminESpitalit/Controllers/DoctorsController.cs
+++ SistemPerMenaxhiminESpitalit/SistemPerMenaxhiminESpitalit/Controllers/DoctorsController.cs
@@ -15,12 +15,13 @@ namespace SistemPerMenaxhiminESpitalit.Controllers
     {
         private readonly ILogger<DoctorsController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
+        ApplicationDbContext _context;
 
-
-        public DoctorsController(ILogger<DoctorsController> logger, UserManager<ApplicationUser> userManager)
+        public DoctorsController(ILogger<DoctorsController> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
             _logger = logger;
             _userManager = userManager;
+            _context = context;
         }
 
         [HttpGet("get-doctors")]
@@ -32,14 +33,52 @@ namespace SistemPerMenaxhiminESpitalit.Controllers
         [HttpDelete("delete-doctor/{id}")]
         public async Task<IActionResult> DeleteDoctors(string id)
         {
-            try{
+            try {
                 ApplicationUser user = await _userManager.FindByIdAsync(id);
                 string roleName = "doctor";
                 return Ok(await _userManager.DeleteAsync(user));
-            }catch(Exception ex)
+            } catch (Exception ex)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(id);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut(template: "{id}")]
+        public async Task<IActionResult> UpdateDoctors(string id, [FromBody] RegisterModel model)
+        {
+            try
+            {
+                var spec = await _context.specialisations.FindAsync(model.Specialisationid);
+                var user = await _context.Users.FindAsync(id);
+                user.Name = model.Name;
+                user.Email = model.Email;
+                user.Surename = model.Surname;
+                user.Address = model.Address;
+                user.SpecialisationId = spec.SpecialisationId;
+                user.Specialisation = spec;
+                await _context.SaveChangesAsync();
+                return Ok("success");
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
     }
 }
